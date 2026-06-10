08688335f7e07e94cd6b34c0cd427a87083a1b62
Fibelius <jouko@nordledger.com>
2026-06-10 19:20:14 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,12 +2,8 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
 }
-
 module.exports = nextConfig
